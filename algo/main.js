6f3ede1d9e802ab3c80f1b6e65632b4b74040f3e
JossuaT <162606165+JossuaT@users.noreply.github.com>
2024-05-28 17:36:34 +0200

    Added an explanation for main.js

--- algo/main.js
+++ algo/main.js
@@ -1,4 +1,5 @@
 function twoSum(nums, target) {
+    // We use i and j as index counters
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
